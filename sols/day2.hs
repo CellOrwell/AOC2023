8588a7dfc13b0b21f9ab49f8acd6ca8dc121497d
CellOrwell <joshuamilroy@hotmail.co.uk>
2023-12-02 13:03:41 +0000

    add day2 part2

--- sols/day2.hs
+++ sols/day2.hs
@@ -4,15 +4,18 @@ import Data.Char
 gameLimits :: (Int, Int, Int)
 gameLimits = (12, 13, 14)
 
-isGameBig :: [String] -> (Int, Int, Int) -> Bool
-isGameBig (x:y:xs) (r, g, b)
-    | arg == 'r' = (int > r) || isGameBig xs (r, g, b)
-    | arg == 'g' = (int > g) || isGameBig xs (r, g, b)
-    | arg == 'b' = (int > b) || isGameBig xs (r, g, b)
+cubePower :: (Int, Int, Int) -> Int
+cubePower (a, b, c) = a * b * c 
+
+gameLims :: [String] -> (Int, Int, Int) -> (Int, Int, Int)
+gameLims (x:y:xs) (r, g, b)
+    | arg == 'r' = if int > r then gameLims xs (int, g, b) else gameLims xs (r, g, b)
+    | arg == 'g' = if int > g then gameLims xs (r, int, b) else gameLims xs (r, g, b)
+    | arg == 'b' = if int > b then gameLims xs (r, g, int) else gameLims xs (r, g, b)
     where arg = head y
           int = read x
-isGameBig [] (r, g, b) = False
-isGameBig (x:xs) (r, g, b) = False
+gameLims [] (r, g, b) = (r, g, b)
+gameLims (x:xs) (r, g, b) = (r, g, b)
 
 getGameInstr :: [String] -> IO (Int, [String])
 getGameInstr (x:y:xs) = return (read (onlyNums y), xs)
@@ -33,9 +36,10 @@ cycle handle = do
         line <- hGetLine handle
         (x, y) <- getGameInstr (words line)
         res <- Main.cycle handle
-        if isGameBig y gameLimits
-            then return (0 + res)
-            else return (x + res)
+        return (cubePower (gameLims y (0,0,0)) + res)
+        -- if isGameBig y gameLimits
+        --     then return (0 + res)
+        --     else return (x + res)
 
 main :: IO ()
 main = do
